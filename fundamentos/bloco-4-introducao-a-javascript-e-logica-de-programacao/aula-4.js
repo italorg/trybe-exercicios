6167397c219ae09ee783003d3581f3aba81381f4
italorg <italoufsb@gmail.com>
2022-04-12 01:42:35 -0300

    tirando as functions de comentario

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-4.js
@@ -1,43 +1,43 @@
-/*
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-*/
+
 
 //parte I - 01
 
-/*
+
 console.log('Bem-vinda,',info.personagem);
-*/
+
 
 //parte I - 02
 
-/*
+
 info.recorrente = 'Sim';
 console.log(info);
-*/
+
 
 //parte I - 03
 
-/*
+
 for(let key in info){
     console.log(key);
 }
-*/
+
 
 //parte I - 04
 
-/*
+
 for(let key in info){
     console.log(info[key]);
 }
-*/
+
 
 //parte I - 05
 
-/*
+
 let info2 = {
     personagem: 'Tio Patinhas',
     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
@@ -53,9 +53,9 @@ for(key in info, info2){
         console.log('Ambos recorrentes');
     }
 }
-*/
 
-/*
+
+
 let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
@@ -68,13 +68,13 @@ let leitor = {
         },
     ],
   };
-*/
+
 //parte I - 06
 
-//console.log("O livro favorito de "+leitor.nome+' '+leitor.sobrenome+" se chama '"+leitor.livrosFavoritos[0].titulo+"'");
+console.log("O livro favorito de "+leitor.nome+' '+leitor.sobrenome+" se chama '"+leitor.livrosFavoritos[0].titulo+"'");
 
 //parte I - 07
-/*
+
 leitor.livrosFavoritos.push({
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
     autor: 'JK Rowling',
@@ -84,11 +84,11 @@ leitor.livrosFavoritos.push({
 //parte I - 08  
 
 console.log("Julia tem "+leitor.livrosFavoritos.length+" livros favoritos");
-*/
+
 
 //parte II - 01
 
-/*
+
 function verificaPalindrome(word) {
     let word2 = [];
     word2 = word.split('');      
@@ -100,11 +100,11 @@ function verificaPalindrome(word) {
     }
 }
 console.log(verificaPalindrome('arara')); // retorno: true
-*/
+
 
 //parte II - 02
 
-/*
+
 function indiceDoMenorValor(array) {
     let maior = array[0];
     let indice = 0;
@@ -117,11 +117,11 @@ function indiceDoMenorValor(array) {
     return indice;
 }
 console.log(indiceDoMenorValor([2, 3, 6, 7, 10, 1])); // retorno: 4
-*/
+
 
 //parte II - 03
 
-/*
+
 function indiceDoMenorValor(array) {
     let menor = array[0];
     let indice = 0;
@@ -134,11 +134,11 @@ function indiceDoMenorValor(array) {
     return indice;
 }
 console.log(indiceDoMenorValor([2, 4, 6, 7, 10, 0, -3])); // retorno: 6
-*/
+
 
 //parte II - 04
 
-/*
+
 function maiorNome(array){
     nomeMaior = array[0];
     for(key in array){
@@ -149,11 +149,11 @@ function maiorNome(array){
     return nomeMaior;
 }
 console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); // retorno: Fernanda
-*/
+
 
 //parte II - 05
 
-/** 
+
 function maisRepete(array){
     let contador1 = 0;
     let contador2 = 0;
@@ -173,11 +173,11 @@ function maisRepete(array){
     return valorQueRepeteMais;
 }
 console.log(maisRepete([2, 3, 2, 5, 8, 2, 3]));
-*/
+
 
 //parte II - 06
 
-/** 
+
 function somatorio(N) {
     let soma = 0;
     for(let i = N; i > 0; i -= 1){
@@ -186,16 +186,16 @@ function somatorio(N) {
     return soma;
 }
 console.log(somatorio(5));
-*/
+
 
 //parte II - 07
 
-/** 
+
 function oFinal(word, ending){
     
     let palavra = word.split('');
     let final = [];
-    for(let i = word.length-1; i >=word.length-ending.length; i -= 1){
+    for(let i = word.length-1; i >= word.length-ending.length; i -= 1){
         final.push(word[i])
     }
     final = final.reverse('')
@@ -206,6 +206,6 @@ function oFinal(word, ending){
     }
 }
 console.log(oFinal('trybe', 'be'));
-*/
+
 
 
